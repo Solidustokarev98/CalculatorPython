57ef34bf33e6e29975c96286dab77a5ea40a4301
Solidustokarev98 <kirill.tokarev1998@yandex.ru>
2023-02-17 16:56:20 +0500

    Add project files.

---
+++ Calculator/Calculator.py
@@ -0,0 +1,7 @@
+print('Calculator')
+x = int(input('Enter your first number: '))
+y = int(input('Enter your second number: '))
+z = int(input('Enter your third number: '))
+
+print('{} + {} * {}= '.format(x, y, z))
+print(x + y* z)
